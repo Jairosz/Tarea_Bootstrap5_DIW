b9d87272619e05b4ea5b7665bfdad25ba3b86e87
Jairosz <Jairosg03@gmail.com>
2023-10-29 23:30:10 +0100

    Animaciones css
    Animaciones

--- WebGym/script.js
+++ WebGym/script.js
@@ -1,10 +1,4 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-60px"; // Oculta el Navbar
-    }
-    prevScrollpos = currentScrollPos;
-}
+document.addEventListener('scroll', function() {
+    const element = document.querySelector('.element');
+    element.style.animationPlayState = 'running';
+}, { once: true });
