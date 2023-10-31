678a7b04c63b61e731040f01821c39357d4137cd
Jairosz <Jairosg03@gmail.com>
2023-10-31 13:23:56 +0100

    a
    a

--- WebGym/script.js
+++ WebGym/script.js
@@ -3,27 +3,3 @@ document.addEventListener('scroll', function() {
     element.style.animationPlayState = 'running';
 }, { once: true });
 
-const carrusel = document.getElementById('carrusel');
-        const imagenes = document.getElementById('imagenes');
-        const anterior = document.getElementById('anterior');
-        const siguiente = document.getElementById('siguiente');
-
-        let indice = 0;
-
-        anterior.addEventListener('click', () => {
-            if (indice > 0) {
-                indice--;
-                actualizarCarrusel();
-            }
-        });
-
-        siguiente.addEventListener('click', () => {
-            if (indice < 2) {
-                indice++;
-                actualizarCarrusel();
-            }
-        });
-
-        function actualizarCarrusel() {
-            imagenes.style.transform = `translateX(-${indice * 400}px)`;
-        }
